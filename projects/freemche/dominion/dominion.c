4a148e12b4ab5389c58f02b65bc276eb7e0db56c
Cheryl Freeman <freemche@oregonstate.edu>
2018-01-17 00:19:47 -0800

    Moved adventurer card effect to its own function, declared in dominion_helpers.h and implemented in dominion.c

--- projects/freemche/dominion/dominion.c
+++ projects/freemche/dominion/dominion.c
@@ -658,6 +658,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   int drawntreasure=0;
   int cardDrawn;
   int z = 0;// this is the counter for the temp hand
+  int cardEffectStatus;	// used in the various card functions to reflect success or failure
   if (nextPlayer > (state->numPlayers - 1)){
     nextPlayer = 0;
   }
@@ -667,25 +668,8 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   switch( card ) 
     {
     case adventurer:
-      while(drawntreasure<2){
-	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-	  shuffle(currentPlayer, state);
-	}
-	drawCard(currentPlayer, state);
-	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-	  drawntreasure++;
-	else{
-	  temphand[z]=cardDrawn;
-	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  z++;
-	}
-      }
-      while(z-1>=0){
-	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	z=z-1;
-      }
-      return 0;
+	    cardEffectStatus = adventurerEffect(state);
+	    return cardEffectStatus;
 			
     case council_room:
       //+4 Cards
@@ -1221,6 +1205,38 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
     }
 	
   return -1;
+}
+
+//  The adventurer card lets you go through your deck revealing and discarding
+//  cards until you have come upon two treasure cards.  These you put into
+//  your hand.
+//  This function returns 0 for success, -1 for failure.  It changes the
+//  values stored in state.
+//
+//  Notes:  I see some problems with this function but for now will just leave
+//  it as it was written in the original cardEffect switch statement.
+int adventurerEffect(struct gameState *state)
+{
+	printf("inside adenturerEffect\n");
+	while(drawntreasure<2){
+		if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
+			shuffle(currentPlayer, state);
+		}
+		drawCard(currentPlayer, state);
+		cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
+		if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+			drawntreasure++;
+		else{
+			temphand[z]=cardDrawn;
+			state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
+			z++;
+		}
+	}
+	while(z-1>=0){
+		state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
+		z=z-1;
+	}
+	return 0;
 }
 
 int discardCard(int handPos, int currentPlayer, struct gameState *state, int trashFlag)

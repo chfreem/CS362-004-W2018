92811e8496c7dc99bcfe8ae9c15b9e242a1afc8b
Cheryl Freeman <freemche@oregonstate.edu>
2018-01-30 00:30:09 -0800

    Adding files that used to be called myAssert

---
+++ projects/freemche/dominion/testingHelpers.h
@@ -0,0 +1,11 @@
+#ifndef _MY_ASSERT_H 
+#define _MY_ASSERT_H
+
+#include <string.h>
+#include "dominion.h"
+
+void myAssert(int conditionA, int conditionB, 
+		char* condATitle, char* condBTitle);
+
+void copyGame(struct gameState *oldGame, struct gameState *newGame);
+#endif

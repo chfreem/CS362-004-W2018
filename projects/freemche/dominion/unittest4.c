8e5a561bb655a8bc2fea102253e249b669792fc5
Cheryl Freeman <freemche@oregonstate.edu>
2018-02-01 00:47:42 -0800

    Added unittest4 with first test in it

---
+++ projects/freemche/dominion/unittest4.c
@@ -0,0 +1,66 @@
+//  ***********************************************************************  //
+//  This is written as part of Assignment 3 in CS362-400, Winter 2018.
+//  Author:  Cheryl Freeman, freemche@oregonstate.edu
+//
+//  This is written to test the function "isGameOver" in the file dominion.c
+//
+//  This unit test is based on code given to us as part of the class
+//  materials, in the file testUpdateCoins.c.  I especially appreciated the
+//  help with the memcpy lines and the memset reminder.
+//
+//  The tests I plan on running for isGameOver()  are:
+//  1--0 province cards
+//  --nonzero province cards and...
+//  3--every supply card is gone
+//  4--the last 3 supply cards checked are gone
+//  5--the last 2 supply cards checked are gone
+//  ***********************************************************************  //
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include "testingHelpers.h"
+#include "rngs.h"
+
+int main()
+{
+	int seed = 20;		//  for random function
+	int numPlayers = 2;
+	int player;
+	int cardToCount;
+	int result;		//  for storage of function return value
+	int i;
+
+	//  I'm using the same cards as in playdom.c
+	int k[10] = {adventurer, gardens, embargo, village, minion, mine,
+			cutpurse, sea_hag, tribute, smithy};
+	struct gameState currentState;
+	struct gameState storedState;	//  For comparison to see if changes
+					//  were made
+	//  The following array tells the checkStateDifferences()
+	//  function which components of the game state should be
+	//  checked.  
+	int checkFlags[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+
+	//  Test #1
+	printf("\nProvince cards are gone\n");
+	memset(&currentState, '\0', sizeof(struct gameState));
+	memset(&storedState, '\0', sizeof(struct gameState));
+	result = initializeGame(numPlayers, k, seed, &currentState); 
+	if (result < 0)
+	{
+		printf("Game initialization not successful.\n");
+		return -1;
+	}
+
+	//  Now we want to change the game state for
+	//  our testing conditions
+	currentState.supplyCount[province] = 0;
+	copyGame(&currentState, &storedState);
+
+	//  Ready to test!
+	result = isGameOver(&currentState);
+	checkStateDifferences(&storedState, &currentState, checkFlags);
+	myAssert(1, result,"isGameOver");
+	return 0;
+}

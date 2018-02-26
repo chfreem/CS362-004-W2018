829ca809ad6e30455dcb5a2b9bd1632a48551bb5
Cheryl Freeman <freemche@oregonstate.edu>
2018-02-26 00:29:52 -0800

    Added testingHelpers.c and .h

---
+++ projects/freemche/packduDominion/testingHelpers.h
@@ -0,0 +1,21 @@
+#ifndef _MY_ASSERT_H 
+#define _MY_ASSERT_H
+
+#include <string.h>
+#include "dominion.h"
+
+void myAssert(int conditionA, int conditionB, 
+		char* condTitle);
+
+void copyGame(struct gameState *oldGame, struct gameState *newGame);
+
+void checkStateDifferences(struct gameState *oldGame, 
+		struct gameState *newGame, 
+		int checkMemberFlags[NUM_GAMESTATE_MEMBERS]);
+
+int changed(int old, int new);
+
+void countCardTypes(int* cardTypeTotals, struct gameState *state, int player);
+
+int numSpecificCardsInHand(struct gameState *state, int player, int cardToCount);
+#endif
